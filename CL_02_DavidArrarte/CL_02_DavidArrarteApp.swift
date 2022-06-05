9977e8a324be8dd952728a29b8ba6f9f27c5b976
David Arrarte <david.arrarte@banexcoin.com>
2022-06-05 13:17:30 -0500

    Initial Commit

---
+++ CL_02_DavidArrarte/CL_02_DavidArrarteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CL_02_DavidArrarteApp.swift
+//  CL_02_DavidArrarte
+//
+//  Created by David Arrarte on 5/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CL_02_DavidArrarteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CL_02_DavidArrarte/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CL_02_DavidArrarte
+//
+//  Created by David Arrarte on 5/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
